57cb8868bba2224655b6216cda2bdb08204529a6
dharsha2005 <baladharshan1972@gmail.com>
2026-01-24 20:05:29 +0530

    Frontend commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import axios from 'axios';
+
+// Set the base URL for all Axios requests
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'https://qrgaanicc.onrender.com';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
